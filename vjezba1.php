98482a7d3f8247245352ec74e200c4a4293134d6
larastrus <lstrus@tvz.hr>
2026-03-24 19:58:05 +0100

    Add files via upload

---
+++ vjezba1.php
@@ -0,0 +1,16 @@
+<?php
+$naslov = "Moja prva PHP stranica";
+$autor = "Lara Štrus";
+
+echo "<h1>$naslov</h1>";
+echo "<p>Ovu stranicu izradila je <strong>$autor</strong>.</p>";
+echo '<a href="https://visitvukovar.hr/" target="_blank">Posjeti stranicu Turistička zajednica grada Vukovara</a>'
+?>
+
+<!DOCTYPE html>
+<html>
+    <head></head>
+    <body></body>
+</html>
+
+<!-- Naziv datoteke: vjezba1.php -->
